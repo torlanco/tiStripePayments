88b864a1ddded586b22b73e53a50a2c716bf5d09
Duy Bao Nguyen <bduyng@gmail.com>
2017-05-09 16:50:46 +0700

    add icon font and enable icon on card list item

--- controllers/select.js
+++ controllers/select.js
@@ -4,9 +4,20 @@ stripe.fetchCards(stripe.getStripeId());
 
 console.log(Alloy.Collections.cards.toJSON());
 
+var cardIconCodes = {
+    'Visa': '\uF1F0',
+    'Mastercard': '\uF1F1',
+    'American Express': '\uF1F3',
+    'Discover': '\uF1F2',
+    'Diners Club': '\uF24C',
+    'JCB': '\uF24B'
+}
+
 function transformCard(model) {
     var transformed = model.toJSON();
     // TODO: add icon of card here based on transformed.brand
+    transformed.icon = cardIconCodes[transformed.brand] || '';
+
     transformed.title =
         '\u00B7\u00B7\u00B7\u00B7 \u00B7\u00B7\u00B7\u00B7 \u00B7\u00B7\u00B7\u00B7 ' + transformed.last4;
     return transformed;
